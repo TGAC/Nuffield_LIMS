cc69c5f4d20b77b161e9d4a233d39e03240ae7bb
Felix Shaw <felix.shaw@earlham.ac.uk>
2024-06-19 13:10:26 +0100

    adding defaults

--- lims/lims_core/models.py
+++ lims/lims_core/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
 
 class Sample(models.Model):
@@ -9,24 +9,24 @@ class Sample(models.Model):
 
 class Extraction(models.Model):
     sample = models.ForeignKey(Sample, on_delete=models.CASCADE)
-    extraction_date = models.DateTimeField('date extracted')
-    extraction_method = models.CharField(max_length=200)
-    isPass = models.BooleanField()
+    extraction_date = models.DateTimeField('date extracted', default=datetime.now())
+    extraction_method = models.CharField(max_length=200, default="")
+    isPass = models.BooleanField(default=False)
 
 class QC(models.Model):
     extraction = models.ForeignKey(Extraction, on_delete=models.CASCADE)
-    qc_date = models.DateTimeField('date qc')
+    qc_date = models.DateTimeField('date qc', default=datetime.now())
     qc_method = models.CharField(max_length=200)
-    qc_result = models.BooleanField()
+    qc_result = models.BooleanField(default=False)
 
     def isPass(self):
         return self.qc_result
 
 class Sequencing(models.Model):
     extraction = models.ForeignKey(Extraction, on_delete=models.CASCADE)
-    sequencing_date = models.DateTimeField('date sequenced')
-    sequencing_method = models.CharField(max_length=200)
-    sequencing_read_length = models.IntegerField()
+    sequencing_date = models.DateTimeField('date sequenced', default=datetime.now())
+    sequencing_method = models.CharField(max_length=200, default="")
+    sequencing_read_length = models.IntegerField(default=0)
 
     def isShortRead(self):
         return self.sequencing_read_length < 10000
--- lims/lims_core/views.py
+++ lims/lims_core/views.py
@@ -4,21 +4,77 @@ from .models import Sample, Extraction, QC, Sequencing
 
 # Create your views here.
 def index(request):
+    """
+    This view function handles the request for the index page of the application.
+
+    Parameters:
+    request (HttpRequest): A Django HttpRequest object representing the incoming HTTP request.
+
+    Returns:
+    HttpResponse: A Django HttpResponse object that renders the 'index.html' template with the context data.
+    """
+
+    # Query all Sample objects from the database
     samples = Sample.objects.all()
+
+    # Prepare the context data for the template. The context is a dictionary where
+    # the key is the name of the variable in the template, and the value is the actual value you want to pass in.
     context = {"samples": samples}
+
+    # Render the 'index.html' template with the given context data and return the HttpResponse object
     return render(request, 'index.html', context)
 
 def sample_detail(request, sample_id):
+    """
+    This view function handles the request for the detail page of a specific sample.
+
+    Parameters:
+    request (HttpRequest): A Django HttpRequest object representing the incoming HTTP request.
+    sample_id (int): The ID of the sample to display.
+
+    Returns:
+    HttpResponse: A Django HttpResponse object that renders the 'sample_detail.html' template with the context data.
+    """
+
+    # Get the Sample object with the given ID from the database
     detail = Sample.objects.get(pk=sample_id)
+
+    # Query all Extraction objects related to the Sample object from the database
     extractions = detail.extraction_set.all()
+
+    # Prepare the context data for the template. The context is a dictionary where
+    # the key is the name of the variable in the template, and the value is the actual value you want to pass in.
     context = {"sample": detail, "extractions": extractions}
+
+    # Render the 'sample_detail.html' template with the given context data and return the HttpResponse object
     return render(request, 'sample_detail.html', context)
-
 def sample_extraction(request, extraction_id):
-    extraction = Extraction.objects.get(pk=extraction_id)
-    qcs = extraction.qc_set.all()
-    sequencing = extraction.sequencing_set.all()
-    sample = extraction.sample
-    context = {"extraction": extraction, "qcs": qcs, "sequencing": sequencing, "sample": sample}
-    return render(request, 'sample_extraction.html', context)
+    """
+    This view function handles the request for the detail page of a specific extraction.
 
+    Parameters:
+    request (HttpRequest): A Django HttpRequest object representing the incoming HTTP request.
+    extraction_id (int): The ID of the extraction to display.
+
+    Returns:
+    HttpResponse: A Django HttpResponse object that renders the 'sample_extraction.html' template with the context data.
+    """
+
+    # Get the Extraction object with the given ID from the database
+    extraction = Extraction.objects.get(pk=extraction_id)
+
+    # Query all QC objects related to the Extraction object from the database
+    qcs = extraction.qc_set.all()
+
+    # Query all Sequencing objects related to the Extraction object from the database
+    sequencing = extraction.sequencing_set.all()
+
+    # Get the Sample object related to the Extraction object from the database
+    sample = extraction.sample
+
+    # Prepare the context data for the template. The context is a dictionary where
+    # the key is the name of the variable in the template, and the value is the actual value you want to pass in.
+    context = {"extraction": extraction, "qcs": qcs, "sequencing": sequencing, "sample": sample}
+
+    # Render the 'sample_extraction.html' template with the given context data and return the HttpResponse object
+    return render(request, 'sample_extraction.html', context)
